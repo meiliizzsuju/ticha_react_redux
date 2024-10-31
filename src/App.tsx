f49618598fc7edf923c37ed33d2cc38521e0c186
TichaTin <ticha@arlula.com>
2024-10-31 13:09:07 +1100

    rename DataTableWithModal to DataTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import DataTableWithModal from './components/DataTableWithModal';
+import DataTable from './components/DataTable';
 
 function App() {
   return (
-    <DataTableWithModal/>
+    <DataTable/>
   );
 }
 
